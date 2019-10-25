a764b0bc6db75bae3dff41259215e4f1ee8f70ac
Eduard-Gabriel Manda <eduard.manda@gmail.com>
2019-10-25 12:07:11 +0300

    Initial commit

---
+++ src/main/java/com/ing/tech/FindATutor/FindATutorApplication.java
@@ -0,0 +1,13 @@
+package com.ing.tech.FindATutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindATutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindATutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/tech/FindATutor/FindATutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.tech.FindATutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindATutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
