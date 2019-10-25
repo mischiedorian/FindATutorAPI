b526ffc67480e1c2e936c394ebd274630704eb6e
Eduard-Gabriel Manda <eduard.manda@gmail.com>
2019-10-25 21:03:52 +0300

    Fix issue with recursion

--- src/main/java/com/ing/tech/model/dao/Course.java
+++ src/main/java/com/ing/tech/model/dao/Course.java
@@ -1,19 +1,17 @@
 package com.ing.tech.model.dao;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
+@Getter @Setter
 public class Course {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -35,4 +33,26 @@ public class Course {
     @ManyToMany(mappedBy = "courses")
     @JsonIgnore
     Set<Student> studentSet;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return getDateTime() == course.getDateTime() &&
+                getDuration() == course.getDuration() &&
+                getMaxParticipants() == course.getMaxParticipants() &&
+                getParticipants() == course.getParticipants() &&
+                getId().equals(course.getId()) &&
+                getName().equals(course.getName()) &&
+                getDetails().equals(course.getDetails()) &&
+                getLocation().equals(course.getLocation()) &&
+                getDomain().equals(course.getDomain()) &&
+                getTeacher().equals(course.getTeacher());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDetails(), getLocation(), getDateTime(), getDuration(), getMaxParticipants(), getDomain(), getParticipants(), getTeacher());
+    }
 }
